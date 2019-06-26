fabb978eaa084b6a8cc52a98df11818c6b993231
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-26 18:23:26 +0100

    WIP: added click button methods for carousels infinate toggle

--- components/Carousel/ImageCarousel.js
+++ components/Carousel/ImageCarousel.js
@@ -13,26 +13,56 @@ class ImageSlider {
 
         this.imagesNodeList = this.element.getElementsByTagName('img')
         this.sliderIconNodeList = this.element.querySelectorAll('.slider-icon')
-        console.log(this.imagesNodeList)
-        console.log(this.sliderIconNodeList)
+        // console.log(this.imagesNodeList)
+        // console.log(this.sliderIconNodeList)
+        this.imagesNodeList[this.currentImageIndex].style.display = 'flex';
+        this.sliderIconNodeList[this.currentImageIndex].style.color = 'gray';
+   
+        this.imagesNodeListLength = this.imagesNodeList.length
+        this.indexOfLastImage = this.imagesNodeListLength - 1
+        // console.log(this.indexOfLastImage)    
     }
 
     clickLeftButton(){
-        console.log('left clicked')
+        if (this.currentImageIndex === 0) {
+            this.imagesNodeList[this.currentImageIndex].style.display = 'none'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'lightgray'
+
+            this.currentImageIndex = this.indexOfLastImage
+
+            this.imagesNodeList[this.currentImageIndex].style.display = 'flex'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'gray'
+        } else {
+            this.imagesNodeList[this.currentImageIndex].style.display = 'none'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'lightgray'
+
+            this.currentImageIndex = this.currentImageIndex - 1
+
+            this.imagesNodeList[this.currentImageIndex].style.display = 'flex'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'gray'
+        }
     }
 
     clickRightButton(){
-        console.log('right clicked')
+        if (this.currentImageIndex === this.indexOfLastImage) {
+            this.imagesNodeList[this.currentImageIndex].style.display = 'none'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'lightgray'
+
+            this.currentImageIndex = 0
+
+            this.imagesNodeList[this.currentImageIndex].style.display = 'flex'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'gray'
+        } else {
+            this.imagesNodeList[this.currentImageIndex].style.display = 'none'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'lightgray'
+
+            this.currentImageIndex = this.currentImageIndex + 1
+
+            this.imagesNodeList[this.currentImageIndex].style.display = 'flex'
+            this.sliderIconNodeList[this.currentImageIndex].style.color = 'gray'
+        }
     }
 }
-
-
-
-
-
-
-
-
 
 let imageSlider = document.querySelectorAll('.image-slider')
 // console.log(imageSlider);
