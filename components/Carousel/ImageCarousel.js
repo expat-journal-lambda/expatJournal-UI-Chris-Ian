e2c6901bd153c9248c9bb694b2a00a1b198ad26f
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-06-26 21:40:10 +0100

    WIP: added comments to ImageCarousel.js

--- components/Carousel/ImageCarousel.js
+++ components/Carousel/ImageCarousel.js
@@ -1,28 +1,45 @@
 class ImageSlider {
     constructor(element){
+        // assign this.element to the element DOM reference
         this.element = element
+
+        // assign the left and right slider buttons
         this.leftButton = this.element.querySelector('.left-button')
         this.rightButton = this.element.querySelector('.right-button')
-        // console.log(this.leftButton)
-        // console.log(this.rightButton)
-
+        
+        // add 'click' event listeners to slider buttons 
         this.leftButton.addEventListener('click', () => this.clickLeftButton())
         this.rightButton.addEventListener('click', () => this.clickRightButton())
     
+        // set default index for current image in slider
         this.currentImageIndex = 0;
 
+        // create NodeList for images in slider
         this.imagesNodeList = this.element.getElementsByTagName('img')
+
+        // create NodeList for slider icons between buttons that represent each image in slider
         this.sliderIconNodeList = this.element.querySelectorAll('.slider-icon')
-        // console.log(this.imagesNodeList)
-        // console.log(this.sliderIconNodeList)
+
+        // set css properties of current index of NodeList of images to 'flex' and '1' to display default image 
         this.imagesNodeList[this.currentImageIndex].style.display = 'flex';
         this.imagesNodeList[this.currentImageIndex].style.opacity = '1';
+
+        // set css property of current index of NodeList of icons to 'gray' to show which image is 'active/displayed'
         this.sliderIconNodeList[this.currentImageIndex].style.color = 'gray';
    
+        // define the length of of the images NodeList
         this.imagesNodeListLength = this.imagesNodeList.length
+
+        // define the index of the last image in NodeList of images
         this.indexOfLastImage = this.imagesNodeListLength - 1
-        // console.log(this.indexOfLastImage)    
+        
     }
+
+    // method for left button click to remove custom css properties of active image & icon, 
+    // then decrease the index of current image by 1,
+    // and add cutom css properties to image & icon of new current index
+    // if at start of nodelist of Images, set current index to that of last image in NodeList
+    // added GreenSock animations to slide image in from left, while increasing its opacity from 0 to 1
 
     clickLeftButton(){
         if (this.currentImageIndex === 0) {
@@ -49,6 +66,13 @@ class ImageSlider {
             this.sliderIconNodeList[this.currentImageIndex].style.color = 'gray'
         }
     }
+
+
+    // method for right button click to remove custom css properties of active image & icon, 
+    // then increase the index of current image by 1,
+    // and add cutom css properties to image & icon of new current index
+    // if at end of nodelist of Images, set current index to that of first image in NodeList
+    // added GreenSock animations to slide image in from left, while increasing its opacity from 0 to 1
 
     clickRightButton(){
         if (this.currentImageIndex === this.indexOfLastImage) {
@@ -77,7 +101,8 @@ class ImageSlider {
     }
 }
 
+// grab reference to image slider
 let imageSlider = document.querySelectorAll('.image-slider')
-// console.log(imageSlider);
 
+// return a new instance of ImageSlider for each imageSlider
 imageSlider.forEach(carousel => new ImageSlider(carousel))
